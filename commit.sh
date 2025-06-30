efbcaa0a32693b81b67e1f6afb1a5e483a13504d
profandrevsilva <133996898+profandrevsilva@users.noreply.github.com>
2025-06-30 23:56:37 +0000

    getdata notebook

---
+++ commit.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+# script to commit to repo.
+echo "${1}"
+
+echo "Add changes in files or directory..."
+git add .
+echo "committing..."
+git commit -m "${1}"
+git pull origin master
+git push -u origin master
+echo "All done! :)"
