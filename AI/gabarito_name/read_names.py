c7607592c8a97096279475791c92c60014e716b2
Andre V Silva <andsilvadrcc@gmail.com>
2025-09-20 23:13:01 -0300

    done get name from file

--- AI/gabarito_name/read_names.py
+++ AI/gabarito_name/read_names.py
@@ -3,33 +3,39 @@
 
 # Description:
 ####################################
-
-name_classe = 'names_1A'
-
 import pandas as pd
 
-# Load the Excel file
-excel_file = f'classes_names/{name_classe}.xlsx'  # Replace with your file path
-df = pd.read_excel(excel_file, sheet_name=0)  # You can specify the sheet name
+name_classes  = ['names_1A', 'names_1B', 'names_1C', 'names_1D', 'names_1E', 'names_1F', 'names_1G']
 
-# Save as CSV
-csv_file = f'classes_names/{name_classe}.csv'  # Replace with desired CSV path
-df.to_csv(csv_file, index=False)  # index=False removes the row numbers
+for name_classe in name_classes:
+    # Load the Excel file
+    excel_file = f'classes_names/{name_classe}.xlsx'  # Replace with your file path
+    df = pd.read_excel(excel_file, sheet_name=0)  # You can specify the sheet name
 
-df = pd.read_csv(f'classes_names/{name_classe}.csv', sep=',')
+    # Save as CSV
+    csv_file = f'classes_names/{name_classe}.csv'  # Replace with desired CSV path
+    df.to_csv(csv_file, index=False)  # index=False removes the row numbers
 
-n = 8
-df = df.iloc[n:]
+    df = pd.read_csv(f'classes_names/{name_classe}.csv', sep=',')
 
-df = df.drop(columns=['Relatório de Consulta de Fechamento', 
-       'Unnamed: 3', 'Unnamed: 4', 'Unnamed: 5', 'Unnamed: 6', 'Unnamed: 7',
-       'Unnamed: 8', 'Unnamed: 9', 'Unnamed: 10', 'Unnamed: 11'])
+    n = 8
+    df = df.iloc[n:]
 
-df.columns = ['Situacao', 'Nome']
+    df = df.drop(columns=['Relatório de Consulta de Fechamento', 
+        'Unnamed: 3', 'Unnamed: 4', 'Unnamed: 5', 'Unnamed: 6', 'Unnamed: 7',
+        'Unnamed: 8', 'Unnamed: 9', 'Unnamed: 10', 'Unnamed: 11'])
 
-rm_names = ['Baixa - Transferência', 'Transferido', 'Remanejamento', 'Não Comparecimento']
+    df.columns = ['Situacao', 'Nome']
 
-for name in rm_names:
-    df = df[df['Situacao'] != name]
+    rm_names = ['Baixa - Transferência', 'Transferido', 'Remanejamento', 'Não Comparecimento']
 
-print(df)
+    for name in rm_names:
+        df = df[df['Situacao'] != name]
+
+    df = df.drop(columns=['Situacao'])
+
+    print(df)
+
+    name_classe = 'final_' + name_classe
+
+    df.to_csv(f'classes_names/{name_classe}.csv', index=False)
